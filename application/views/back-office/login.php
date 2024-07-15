b2f057982ac2b6f0f461119639f7a70002dd0e79
Kevin-rm <kevinramarozatovo@gmail.com>
2024-07-15 14:25:51 +0300

    Update login.php

--- application/views/back-office/login.php
+++ application/views/back-office/login.php
@@ -17,11 +17,6 @@
 
     <link href="<?= base_url('assets/vendor/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
     <link href="<?= base_url('assets/vendor/bootstrap-icons/bootstrap-icons.css') ?>" rel="stylesheet">
-    <link href="<?= base_url('assets/vendor/boxicons/css/boxicons.min.css') ?>" rel="stylesheet">
-    <link href="<?= base_url('assets/vendor/quill/quill.snow.css') ?>" rel="stylesheet">
-    <link href="<?= base_url('assets/vendor/quill/quill.bubble.css') ?>" rel="stylesheet">
-    <link href="<?= base_url('assets/vendor/remixicon/remixicon.css') ?>" rel="stylesheet">
-    <link href="<?= base_url('assets/vendor/simple-datatables/style.css') ?>" rel="stylesheet">
 
     <link href="<?= base_url('assets/css/style.css') ?>" rel="stylesheet">
 </head>
