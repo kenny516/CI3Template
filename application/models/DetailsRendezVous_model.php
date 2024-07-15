1635866d95a5803bc3327d447b50699c270e38db
Rihantiana Mbolatsiory <108069670+LegendaryGhost@users.noreply.github.com>
2024-07-15 22:12:56 +0300

    Modèle des détails rendez-vous 2

--- application/models/DetailsRendezVous_model.php
+++ application/models/DetailsRendezVous_model.php
@@ -3,46 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class DetailsRendezVous_model extends CI_Model {
 
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->model('Service_model');
-        $this->load->model('Ouverture_model');
-        date_default_timezone_set('Europe/Paris'); // Set the timezone here
-    }
-
-    /**
-     * @param $data = array(
-     *      'id_voiture' => 1,
-     *      'id_service' => 2,
-     *      'id_slot' => 3,
-     *      'date_debut' => '2023-03-01 10:00:00',
-     *      'date_paiement' => null
-     * );
-     * @return mixed
-     */
-    public function insert($data) {
-        $details = $this->DetailsRendezVous_model->get_details($data);
-
-        // Debugging output
-        echo '<pre>';
-        var_dump($details);
-        echo '</pre>';
-        die();
-
-        // Insert the rendez_vous
-        $this->db->insert('garage_auto_rendez_vous', $data);
-        $rendez_vous_id = $this->db->insert_id();
-
-        // Insert the details with the correct rendez_vous ID
-        foreach ($details as &$detail) {
-            $detail['id_rendez_vous'] = $rendez_vous_id;
-            $this->db->insert('garage_auto_details_rendez_vous', $detail);
-        }
-
-        return $rendez_vous_id;
-    }
-
     public function get_details($data)
     {
         // Extract the service duration into a DateInterval instance
