58d3d7a87315ac0c6f3904be8b1cbc6de91b31da
Kevin-rm <kevinramarozatovo@gmail.com>
2024-07-15 18:04:21 +0300

    vita

--- application/views/back-office/services/list.php
+++ application/views/back-office/services/list.php
@@ -2,7 +2,13 @@
     <div class="col-12">
         <div class="card">
             <div class="card-body">
-                <h5 class="card-title">Liste</h5>
+                <div class="d-flex justify-content-between align-items-center">
+                    <h5 class="card-title">Liste</h5>
+                    <a type="button" href="#" class="btn btn-primary">
+                        Ajouter
+                        <i class="bi bi-plus-lg"></i>
+                    </a>
+                </div>
                 <table class="table">
                     <thead>
                     <tr>
@@ -10,6 +16,7 @@
                         <th scope="col">Nom</th>
                         <th scope="col">Durée</th>
                         <th scope="col">Prix</th>
+                        <th scope="col">Actions</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -19,6 +26,17 @@
                             <td><?= $service['nom'] ?></td>
                             <td><?= $service['duree'] ?></td>
                             <td><?= $service['prix'] ?></td>
+                            <td>
+                                <div class="dropdown">
+                                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <i class="bi bi-three-dots-vertical"></i>
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="#">Modifier</a></li>
+                                        <li><a class="dropdown-item" href="#">Supprimer</a></li>
+                                    </ul>
+                                </div>
+                            </td>
                         </tr>
                     <?php endforeach; ?>
                     </tbody>
