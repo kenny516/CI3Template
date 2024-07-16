8813816e19e9a77555099b7fb2d6a58d4f03d09e
Rihantiana Mbolatsiory <108069670+LegendaryGhost@users.noreply.github.com>
2024-07-16 16:11:56 +0300

    FO appointment validation

--- application/controllers/front-office/Appointment.php
+++ application/controllers/front-office/Appointment.php
@@ -6,9 +6,12 @@ class Appointment extends CI_Controller
     public function __construct() {
         parent::__construct();
         $this->load->library('form_validation');
+        $this->load->helper('validation');
         $this->load->model('Service_model');
         $this->load->model('Slot_model');
         $this->load->model('DetailsRendezVous_model');
+        $this->load->model('Ouverture_model');
+        $this->load->model('RendezVous_model');
     }
 
     public function index() {
@@ -18,20 +21,23 @@ class Appointment extends CI_Controller
     }
 
     public function submit() {
-        // Set validation rules
+        $ouvertures = $this->Ouverture_model->get_all();
+        $ouverture = end($ouvertures);
+        $heure_ouverture = $ouverture['ouvert'];
+        $heure_fermeture = $ouverture['fermer'];
+
         $this->form_validation->set_rules('service', 'Service', 'required');
         $this->form_validation->set_rules('date-debut', 'Date de début', 'required');
-        $this->form_validation->set_rules('heure-debut', 'Heure de début', 'required');
+        $this->form_validation->set_rules('heure-debut', 'Heure de début', 'required|validation_heure_debut[' . $heure_ouverture . ',' . $heure_fermeture . ']');
 
         $client = $this->session->userdata("client");
 
-        if(!$client){
+        if (!$client) {
             redirect('front-office/Login');
             return;
         }
 
         if (!$this->form_validation->run()) {
-            // Validation failed, load the form again with errors
             $data['content'] = FRONT_OFFICE_VIEW_FOLDER . 'appointment';
             $data['services'] = $this->Service_model->get_all();
             $this->load->view(FRONT_OFFICE_VIEW_FOLDER . 'templates/base_layout', $data);
@@ -42,8 +48,7 @@ class Appointment extends CI_Controller
         $service = $this->input->post('service');
         $dateDebut = $this->input->post('date-debut') . ' ' . $this->input->post('heure-debut');
         $format = 'm/d/Y h:i A';
-        $dateDebut = DateTime::createFromFormat($format, $dateDebut);
-        $dateDebut = $dateDebut->format('Y-m-d H:i:s');
+        $dateDebut = DateTime::createFromFormat($format, $dateDebut)->format('Y-m-d H:i:s');
         $data = array(
             'id_service' => $service,
             'date_debut' => $dateDebut
@@ -51,7 +56,7 @@ class Appointment extends CI_Controller
 
         $details = $this->DetailsRendezVous_model->get_details($data);
 
-        $last_elmt = $details[count($details) - 1];
+        $last_elmt = end($details);
         $date_max = $last_elmt['date_details'] . " " . $last_elmt['heure_fin'] . ':00';
         $resultat = $this->DetailsRendezVous_model->get_available_slots($dateDebut, $date_max);
 
