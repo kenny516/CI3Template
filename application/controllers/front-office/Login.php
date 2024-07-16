038cbeef9f2c70dd4d3a7acb23044789c257a422
kenny516 <126701465+kenny516@users.noreply.github.com>
2024-07-16 09:59:23 +0300

    login front

--- application/controllers/front-office/Login.php
+++ application/controllers/front-office/Login.php
@@ -7,16 +7,18 @@ class Login extends CI_Controller
     {
         parent::__construct();
         $this->load->helper('form');
+        $this->load->model("Type_voiture_model");
     }
 
     public function index()
     {
         $data['content'] = FRONT_OFFICE_VIEW_FOLDER . 'login';
+        $data['donne'] = $this->Type_voiture_model->get_all();
         $this->load->view(FRONT_OFFICE_VIEW_FOLDER . 'templates/base_layout', $data);
     }
 
     public function submit()
     {
-        // To implement
+
     }
 }
